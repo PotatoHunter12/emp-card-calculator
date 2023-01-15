1dd14c534fffa7fc263e00ad96845eb6b1e8110b
PotatoHunter12 <64433302+PotatoHunter12@users.noreply.github.com>
2023-01-15 18:43:36 +0100

    nav menu added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+    document.getElementById('main').style.marginRight = '-250px';
+  }
+  closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+    document.getElementById('main').style.marginRight = '0';
+  }
 }
